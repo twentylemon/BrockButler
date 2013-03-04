86ee33496408578940e68647e82275c78ea36968
Taras Mychaskiw <t-ross09@hotmail.com>
2013-03-04 12:18:49 -0500

    design updates

--- design/courses/db/create_tables.sql
+++ design/courses/db/create_tables.sql
@@ -2,6 +2,8 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
+CREATE SCHEMA IF NOT EXISTS `brockbutler` DEFAULT CHARACTER SET latin1 COLLATE latin1_general_ci ;
+USE `brockbutler` ;
 
 -- -----------------------------------------------------
 -- Table `brockbutler`.`courses`
@@ -11,6 +13,7 @@ CREATE  TABLE IF NOT EXISTS `brockbutler`.`courses` (
   `code` CHAR(4) NOT NULL ,
   `desc` VARCHAR(45) NULL ,
   `instructor` VARCHAR(45) NULL ,
+  `instructor_email` VARCHAR(45) NULL ,
   PRIMARY KEY (`subj`, `code`) )
 ENGINE = InnoDB;
 
@@ -23,6 +26,7 @@ CREATE  TABLE IF NOT EXISTS `brockbutler`.`courses` (
   `code` CHAR(4) NOT NULL ,
   `desc` VARCHAR(45) NULL ,
   `instructor` VARCHAR(45) NULL ,
+  `instructor_email` VARCHAR(45) NULL ,
   PRIMARY KEY (`subj`, `code`) )
 ENGINE = InnoDB;
 
@@ -52,14 +56,88 @@ ENGINE = InnoDB;
 CREATE  TABLE IF NOT EXISTS `brockbutler`.`offering_times` (
   `id` INT NOT NULL ,
   `day` CHAR(1) NOT NULL ,
-  `timestart` CHAR(4) NULL ,
-  `timeend` CHAR(4) NULL ,
+  `time_start` CHAR(4) NULL ,
+  `time_end` CHAR(4) NULL ,
   `location` VARCHAR(16) NULL ,
   PRIMARY KEY (`id`, `day`) ,
   INDEX `fk_offering_times_offerings1_idx` (`id` ASC) ,
   CONSTRAINT `fk_offering_times_offerings1`
     FOREIGN KEY (`id` )
     REFERENCES `brockbutler`.`offerings` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `brockbutler`.`tasks`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `brockbutler`.`tasks` (
+  `subj` CHAR(4) NOT NULL ,
+  `code` CHAR(4) NOT NULL ,
+  `assign` INT NOT NULL ,
+  `name` VARCHAR(45) NULL ,
+  `mark` INT NULL ,
+  `base` INT NULL ,
+  `weight` FLOAT NULL ,
+  `due` DATETIME NULL ,
+  `create_date` DATETIME NULL ,
+  `priority` INT NULL ,
+  PRIMARY KEY (`assign`, `subj`, `code`) ,
+  INDEX `fk_marks_courses1_idx` (`subj` ASC, `code` ASC) ,
+  CONSTRAINT `fk_marks_courses1`
+    FOREIGN KEY (`subj` , `code` )
+    REFERENCES `brockbutler`.`courses` (`subj` , `code` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `brockbutler`.`contacts`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `brockbutler`.`contacts` (
+  `subj` CHAR(4) NOT NULL ,
+  `code` CHAR(4) NOT NULL ,
+  `cid` INT NOT NULL ,
+  `fname` VARCHAR(45) NULL ,
+  `lname` VARCHAR(45) NULL ,
+  `email` VARCHAR(45) NULL ,
+  INDEX `fk_contacts_courses1_idx` (`subj` ASC, `code` ASC) ,
+  PRIMARY KEY (`subj`, `cid`, `code`) ,
+  CONSTRAINT `fk_contacts_courses1`
+    FOREIGN KEY (`subj` , `code` )
+    REFERENCES `brockbutler`.`courses` (`subj` , `code` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `brockbutler`.`task_contacts`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `brockbutler`.`task_contacts` (
+  `subj` CHAR(4) NOT NULL ,
+  `code` CHAR(4) NOT NULL ,
+  `cid` INT NOT NULL ,
+  `assign` INT NOT NULL ,
+  PRIMARY KEY (`subj`, `code`, `cid`, `assign`) ,
+  INDEX `fk_task_contacts_contacts1_idx` (`cid` ASC) ,
+  INDEX `fk_task_contacts_tasks1_idx` (`assign` ASC) ,
+  INDEX `fk_task_contacts_courses1_idx` (`subj` ASC, `code` ASC) ,
+  CONSTRAINT `fk_task_contacts_contacts1`
+    FOREIGN KEY (`cid` )
+    REFERENCES `brockbutler`.`contacts` (`cid` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_task_contacts_tasks1`
+    FOREIGN KEY (`assign` )
+    REFERENCES `brockbutler`.`tasks` (`assign` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_task_contacts_courses1`
+    FOREIGN KEY (`subj` , `code` )
+    REFERENCES `brockbutler`.`courses` (`subj` , `code` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
