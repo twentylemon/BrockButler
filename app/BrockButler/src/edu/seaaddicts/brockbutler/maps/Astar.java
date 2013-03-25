e0e68c8987dc033002a977a07ed2e387715885dc
Thomas Nelson <tn90ca@gmail.com>
2013-03-25 18:10:17 -0400

    Added path finding classes to application, not connected yet

---
+++ app/BrockButler/src/edu/seaaddicts/brockbutler/maps/Astar.java
@@ -0,0 +1,177 @@
+package edu.seaaddicts.brockbutler.maps;
+
+/**
+ * Astar.java
+ * Brock Butler
+ * Path finding algorithm for navigation route
+ * portion of Brock Butler.
+ * Created by Thomas Nelson 2013-03-05
+ * Copyright (c) 2013 Sea Addicts. All rights reserved.
+ */
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class Astar {
+	
+	/**
+	 * Class variable for the ASTAR class. All are private variables
+	 * as they are all atributited to the database helper and 
+	 * are only used by this class.
+	 */
+	private static final String KEY_NODE = "node_id";
+	private static final String KEY_DESC = "desc";
+	private static final String KEY_XPOS = "x";
+	private static final String KEY_YPOS = "y";
+	private static final String KEY_CONN = "con";
+	private static final String DATABASE_TABLE = "node_connections";
+	
+	private DatabaseHelper ourHelper;
+    private final Context ourContext;
+    private SQLiteDatabase ourDatabase;
+    private Position[] graph;
+
+    
+    /**
+	 * Constructor method for the ASTAR class. The constructor
+	 * is given the context of the calling activity as an argument
+	 * then opens and sets up the database connection. Also creates the
+	 * graph from the database for the searchable space.
+	 */ 
+    public Astar (Context c) {
+    	ourContext = c;
+    	ourHelper = new DatabaseHelper(ourContext);
+        ourDatabase = ourHelper.getWritableDatabase();
+        
+        Cursor cur = ourDatabase.rawQuery("SELECT DISTINCT node_id, desc, x, y FROM node_connections", null);
+    	graph = new Position[cur.getCount()];
+    	if (cur.moveToFirst()) {
+    		int i = 0;
+    		do {
+    			graph[i] = new Position(cur.getInt(cur.getColumnIndex(KEY_XPOS)),cur.getInt(cur.getColumnIndex(KEY_YPOS)),cur.getString(cur.getColumnIndex(KEY_DESC)),cur.getString(cur.getColumnIndex(KEY_NODE)));
+    			i++;
+    		} while (cur.moveToNext());
+    	}
+    	
+    	for(int i=0; i<graph.length; i++) {
+    		String[] columns = new String[]{KEY_NODE,KEY_DESC,KEY_XPOS,KEY_YPOS,KEY_CONN};
+        	cur = ourDatabase.query(DATABASE_TABLE, columns, KEY_CONN+"=?", new String[]{graph[i].nodeNumber}, null, null, null);
+        	if (cur.moveToFirst()) {
+        		graph[i].accesible = new Position[cur.getCount()];
+        		int k=0;
+        		do {
+        			graph[i].accesible[k] = new Position(cur.getInt(cur.getColumnIndex(KEY_XPOS)), cur.getInt(cur.getColumnIndex(KEY_YPOS)), cur.getString(cur.getColumnIndex(KEY_DESC)), cur.getString(cur.getColumnIndex(KEY_NODE)));
+        			for(int j=0; j<graph.length; j++) {
+        				if(graph[i].accesible[k].compare(graph[j]))
+        					graph[i].accesible[k] = graph[j];
+        			}
+        			k++;
+        		} while(cur.moveToNext());
+        	}
+    	}
+    }
+    
+    
+    /**
+	 * The pathGeneration method is the main part of the A* algorithm. This method
+	 * achieves an efficient and route between two positions based on a heuristic 
+	 * score.
+	 */ 
+    public Position[] pathGeneration(Position startNode, Position goalNode)  {
+    	for(int j=0; j<graph.length; j++) {
+			if(startNode.compare(graph[j]))
+				startNode = graph[j];
+			else if(goalNode.compare(graph[j]))
+				goalNode = graph[j];
+		}
+		
+		if(startNode.accesible.length == 0 || goalNode.accesible.length == 0)
+    		return null;
+    	
+        PriorityQueue<Position> openList = new PriorityQueue<Position>();
+        openList.add(startNode);
+        
+        while(openList.size() != 0)  {
+        	
+        	Position tempNode = openList.remove();
+            
+            if(tempNode == goalNode) 
+            	return pathReturn(tempNode);
+            
+            tempNode.visited = true;
+            tempNode.gScore  = 0;
+            
+            double tempX = tempNode.xPosition - goalNode.xPosition;
+            double tempY = tempNode.yPosition - goalNode.yPosition;
+            tempNode.hScore = Math.sqrt(tempX*tempX + tempY*tempY);
+
+            for(int i=0; i<tempNode.accesible.length; i++) {
+            	Position adjacentNode = tempNode.accesible[i];
+            	
+                if(adjacentNode.visited) 
+                	continue;
+                
+                double tempG = tempNode.gScore;
+                double x = tempNode.xPosition - adjacentNode.xPosition;
+                double y = tempNode.yPosition - adjacentNode.yPosition;
+                double dist = Math.sqrt(x*x + y*y);
+                tempG += dist;
+               
+                if(tempG < adjacentNode.gScore ) {
+                	openList.remove(adjacentNode);
+                	adjacentNode.from = tempNode;
+                	adjacentNode.gScore = tempG;
+
+                	tempX = adjacentNode.xPosition - goalNode.xPosition;
+                	tempY = adjacentNode.yPosition - goalNode.yPosition;
+
+
+                	adjacentNode.hScore = Math.sqrt(tempX*tempX + tempY*tempY);
+                	adjacentNode.fScore = adjacentNode.gScore + adjacentNode.hScore;
+                	adjacentNode.visited = true;
+                    openList.add(adjacentNode);
+                }
+
+            }
+        }
+        return null;
+    }
+    
+    
+    /**
+	 * This method takes the generated route from the A* algorithm
+	 * and processes it into a usable ArrayList of positions to be 
+	 * passed to the mapping activity for drawing a route on the
+	 * map.
+	 */ 
+    public Position[] pathReturn(Position end) throws ClassCastException {
+        ArrayList<Position> routeList = new ArrayList<Position>();
+        routeList.add(end);
+
+        while(end.from != null) {
+            end = end.from;
+            routeList.add(end);
+        }
+        
+        Object tempRoute[] = (routeList.toArray());
+        Position route[] = new Position[tempRoute.length];
+        
+        for(int i=0; i<route.length; i++) {
+            route[i] = (Position)(tempRoute[route.length - i-1]);
+        }
+        
+        return route;
+    }
+    
+    
+    /**
+	 * This method is invoked to close the connection to the database once the
+	 * path has been generated.
+	 */ 
+    public void close(){
+        ourHelper.close();
+    }
+}
---
+++ app/BrockButler/src/edu/seaaddicts/brockbutler/maps/Position.java
@@ -0,0 +1,184 @@
+package edu.seaaddicts.brockbutler.maps;
+
+/**
+ * Position.java
+ * Brock Butler
+ * Type for holding Position node 
+ * portion of Brock Butler.
+ * Created by Thomas Nelson 2013-03-05
+ * Copyright (c) 2013 Sea Addicts. All rights reserved.
+ */
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+public class Position implements Comparable<Object> {
+	
+	/**
+	 * Class variable for the POSITION class. All are public
+	 * to avoid using get/set variables to increase performance
+	 */ 
+	public int      xPosition;
+	public int      yPosition;
+	public double   fScore;
+	public double   gScore;
+	public double   hScore;
+	public String   nodeNumber;
+	public String   nodeName;
+	public boolean  visited;
+    public Position from;
+    public Position accesible[];
+    public Position nonaccesible[];
+	
+	/**
+	 * Constructor methods for the POSITION class. The constructor
+	 * is overloaded 3 times for 3 different way to initialize a 
+	 * POSITION.
+	 */ 
+	public Position ( ) {
+		xPosition = 0;
+		yPosition = 0;
+		
+		nodeNumber = "";
+		nodeName   = "";
+		
+		fScore = Double.MAX_VALUE;
+        gScore = Double.MAX_VALUE;
+        hScore = -1;
+        
+        visited = false;
+        from    = null;
+	}
+	
+	public Position (int inputX, int inputY) {
+		xPosition = inputX;
+		yPosition = inputY;
+		
+		nodeNumber = "";
+		nodeName   = "";
+		
+		fScore = Double.MAX_VALUE;
+		gScore = Double.MAX_VALUE;
+		hScore = Double.MAX_VALUE;
+        
+		visited = false;
+        from    = null;
+	}
+	
+	public Position (int inputX, int inputY, String inputName, String inputNumber) {
+		xPosition = 0;
+		yPosition = 0;
+		
+		nodeNumber = inputNumber;
+		nodeName   = inputName;
+		
+		fScore = Double.MAX_VALUE;
+		gScore = Double.MAX_VALUE;
+		hScore = Double.MAX_VALUE;
+        
+		visited = false;
+        from    = null;
+	}
+
+	
+	/**
+	 * Methods for the POSITION class. These methods are provided to
+	 * get and set the class variables although for performance reasons they
+	 * will be used very rarely. The final method is able to compare this position
+	 * to another with a boolean return.
+	 */ 
+	public void setCoordinates (int inputX, int inputY) {
+		xPosition  = inputX;
+		yPosition  = inputY;
+	}
+	
+	public void setNumber (String inputNumber) {
+		nodeNumber  = inputNumber;
+	}
+	
+	public void setName (String inputName) {
+		nodeName  = inputName;
+	}
+	
+	public int getX ( ) {
+		return xPosition;
+	}
+	
+	public int getY ( ) {
+		return yPosition;
+	}
+	
+	public String getNumber ( ) {
+		return nodeNumber;
+	}
+	
+	public String getName ( ) {
+		return nodeName;
+	}
+	
+	public boolean compare (Position node) {
+		if(this.xPosition == node.xPosition && this.yPosition == node.yPosition && this.nodeNumber.equals(node.nodeNumber) && this.nodeName.equals(node.nodeName))
+			return true;
+		return false;
+	}
+	
+	/**
+	 * The following method is used only to search coordinates
+	 * from a room number
+	 */
+	public void fillFromName(Context ourContext) {
+		DatabaseHelper ourHelper = new DatabaseHelper(ourContext);
+	    SQLiteDatabase ourDatabase = ourHelper.getReadableDatabase();
+	    
+	    String[] columns = new String[]{"node_id","x","y"};
+    	Cursor cur = ourDatabase.query("node_connections", columns, "desc"+"=?", new String[]{nodeName}, null, null, null);
+    	if (cur.moveToFirst()) {
+    		nodeNumber = cur.getString(cur.getColumnIndex("node_id"));
+    		xPosition = cur.getInt(cur.getColumnIndex("x"));
+    		yPosition = cur.getInt(cur.getColumnIndex("y"));
+    	} else {
+    		Log.e("POSITION CLASS", "Can't find room data");
+    	}
+	    
+	    ourHelper.close();
+	}
+	
+	/**
+	 * gathers all metadata for a node based off of the
+	 * coordinates
+	 */
+	public void fillFromCoordinates(Context ourContext) {
+		DatabaseHelper ourHelper = new DatabaseHelper(ourContext);
+	    SQLiteDatabase ourDatabase = ourHelper.getReadableDatabase();
+	    
+	    String[] columns = new String[]{"node_id","desc"};
+    	Cursor cur = ourDatabase.query("node_connections", columns, "x=? AND y=?", new String[]{Integer.toString(xPosition),Integer.toString(yPosition)}, null, null, null);
+    	if (cur.moveToFirst()) {
+    		nodeNumber = cur.getString(cur.getColumnIndex("node_id"));
+    		nodeName = cur.getString(cur.getColumnIndex("desc"));
+    	} else {
+    		Log.e("POSITION CLASS", "Can't find position data");
+    	}
+	    
+	    ourHelper.close();
+	}
+	
+	
+	/**
+	 * Testing methods for the POSITION class. These methods are provided
+	 * for testing and debugging purposes capable of printing variables to the log
+	 */ 
+	public void printCoordinates ( ) {
+		Log.d("POSITION CLASS", "Coordinates: (" + xPosition + "," + yPosition + ")");
+	}
+	
+	public void printNumber ( ) {
+		Log.d("POSITION CLASS", "Node Number: " + nodeNumber);
+	}
+	
+	public void printName ( ) {
+		Log.d("POSITION CLASS", "Node Name: " + nodeName);
+	}
+}
